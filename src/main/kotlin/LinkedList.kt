84ab1dc44bfa485c0bda362b2de8792db645cc62
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2022-09-23 19:06:54 +0300

    Added templates of API.

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -1,2 +1,23 @@
-class LinkedList {
+class LinkedList<T> {
+    fun insert(index: Int, obj: T) {
+        TODO("Not yet implemented")
+    }
+    fun removeAt(index: Int) {
+        TODO("Not yet implemented")
+    }
+    operator fun get(index: Int) {
+        TODO("Not yet implemented")
+    }
+    operator fun set(index: Int, obj: T) {
+        TODO("Not yet implemented")
+    }
+    fun find(obj: T) {
+        TODO("Not yet implemented")
+    }
+    fun count() {
+        TODO("Not yet implemented")
+    }
+    fun clear() {
+        TODO("Not yet implemented")
+    }
 }
