d5dae9317233e46e56df2a9b9b0f3829195da833
Alexander <106minutes@gmail.com>
2022-09-29 20:53:08 +0300

    add funs: get, insertFirst, add, insert

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -1,6 +1,6 @@
 class LinkedList<T> {
     
-    class Node<T>(var value: T, var nexNode: Node<T>? = null)
+    class Node<T>(var value: T, var nextNode: Node<T>? = null)
     
     private var head: Node<T>? = null
     
@@ -10,18 +10,54 @@ class LinkedList<T> {
     
     fun count(): Int = size
        
-    fun insert(index: Int, obj: T) {
-        TODO("Not yet implemented")
-    }
-    fun removeAt(index: Int) {
-        TODO("Not yet implemented")
-    }
     operator fun get(index: Int): T {
-        TODO("Not yet implemented")
+        var current = head
+        var currentIndex = 0
+        while (current != null && currentIndex < index) {
+            current = current.nextNode
+            currentIndex++
+        }
+        return current
     }
     operator fun set(index: Int, obj: T) {
         TODO("Not yet implemented")
     }
+    
+    fun insertFirst(value: T) {
+        head = Node(value = value, nextNode = head)
+        if (last == null) {
+            last = head
+        }
+        size++
+    }
+
+    fun add(value: T) {
+        if (size==0) {
+            insertFirst(value)
+        }
+        else {
+            last?.nextNode = Node(value = value)
+            last= last?.nextNode
+            size++
+        }
+    }
+    
+    fun insert(index: Int, value: T) {
+        if (size==0 || index == size) {
+            insertFirst(value)
+        }
+        else {
+            var prev = get(index-1)
+            var newNode = Node(value, prev.nextNode)
+            prev.nextNode = newNode
+            size++
+        }
+    }
+    
+    fun removeAt(index: Int) {
+        TODO("Not yet implemented")
+    }
+    
     fun find(obj: T): Int {
         TODO("Not yet implemented")
     }
