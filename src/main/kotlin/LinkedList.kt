11bee1936bbe534af4a1d85803b7c51587530b07
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2022-09-29 15:20:52 +0300

    Add forgotten return types.

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -5,16 +5,16 @@ class LinkedList<T> {
     fun removeAt(index: Int) {
         TODO("Not yet implemented")
     }
-    operator fun get(index: Int) {
+    operator fun get(index: Int): T {
         TODO("Not yet implemented")
     }
     operator fun set(index: Int, obj: T) {
         TODO("Not yet implemented")
     }
-    fun find(obj: T) {
+    fun find(obj: T): Int {
         TODO("Not yet implemented")
     }
-    fun count() {
+    fun count(): Int {
         TODO("Not yet implemented")
     }
     fun clear() {
